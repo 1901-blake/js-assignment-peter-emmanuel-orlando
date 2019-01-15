35402898b0c6b6654245bfc3c843dd6dfd864c23
Emmanuel Orlando <46684214+peter-emmanuel-orlando@users.noreply.github.com>
2019-01-15 09:14:07 -0500

    Update 01-fibonacci.js

--- problems/01-fibonacci/01-fibonacci.js
+++ problems/01-fibonacci/01-fibonacci.js
@@ -1,6 +1,12 @@
 /* 1. Fibonacci
 Define function: fib(n) 
 Return the nth number in the fibonacci sequence. */
-function fib(n) {
-
+function fib(n)
+{
+  let result = 0;
+  for( let i = 0; i <= n; i++)
+  {
+    result += i;
+  }
+  return result;
 }

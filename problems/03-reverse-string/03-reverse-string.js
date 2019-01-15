f02151eea30dfe1f880fb9c09ed96394565de941
Peter Emmanuel Orlando <peter.emmanuel.orlando@gmail.com>
2019-01-15 17:23:00 -0500

    solved reverseing strings problem

--- problems/03-reverse-string/03-reverse-string.js
+++ problems/03-reverse-string/03-reverse-string.js
@@ -1,6 +1,18 @@
 /* 3. Reverse String
 Define function: reverseStr(someStr)
 Reverse and return the String. */
-function reverseStr(someStr) {
-  
+function reverseStr(someStr) 
+{
+    let prev = (someStr + "").split('');
+    let result = [...prev];
+    
+    for( let i = 0,  l = prev.length; i < l  ; i++)
+    {
+        result[Math.abs(i - l) - 1] = prev[i];
+    }
+    return result.join('');
 }
+
+console.log(reverseStr(4321));
+console.log(reverseStr( "4321" ));
+console.log(reverseStr( "PETER EMMANUEL ORLANDO" ));

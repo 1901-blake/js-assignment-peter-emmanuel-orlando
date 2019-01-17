ec13a6373ef7070081cc8eb6ac8c8801c071af7c
Peter Emmanuel Orlando <peter.emmanuel.orlando@gmail.com>
2019-01-17 08:52:29 -0500

    error message in factorial more specific

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -3,7 +3,7 @@ Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) 
 {
-    if(typeof(sumNum) != typeof 1) throw TypeError('only accepts numbers');
+    if(typeof(sumNum) != typeof 1) throw TypeError('factorial only accepts numbers');
     if(sumNum === 1) 
         return 1;
     else

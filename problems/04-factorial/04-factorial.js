759c93a33fc727e9dd48bf08be0a4e9d2817236a
Peter Emmanuel Orlando <peter.emmanuel.orlando@gmail.com>
2019-01-17 09:11:49 -0500

    slight bugfixes

--- problems/04-factorial/04-factorial.js
+++ problems/04-factorial/04-factorial.js
@@ -3,8 +3,9 @@ Define function: factorial(someNum)
 Use recursion to compute and return the factorial of someNum.  */
 function factorial(sumNum) 
 {
-    if(typeof(sumNum) != typeof 1 || sumNum.isInteger) throw TypeError('factorial only accepts integers');
+    if(typeof(sumNum) != typeof 1 || !sumNum.isInteger) throw TypeError('factorial only accepts integers');
     if(sumNum < 1) return NaN;
+
     if(sumNum === 1) 
         return 1;
     else
